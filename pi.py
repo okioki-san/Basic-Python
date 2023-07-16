fcd81a00ca96d308b26ccf073f927b33414663fe
okioki-san <r.oki.23.6.1033@4nonome.com>
2023-07-16 15:48:44 +0900

    Python入門の解答2

--- pi.py
+++ pi.py
@@ -5,5 +5,6 @@ text = """
 
 # TODO
 
+
 list=list((map(len, ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters","involving", "quantum", "mechanics", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard"])))
 print(*list, sep="")
