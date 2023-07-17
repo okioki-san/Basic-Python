f794c4c1aa66e6569b5073719e93010303c58ae0
okioki-san <r.oki.23.6.1033@4nonome.com>
2023-07-17 12:35:13 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,24 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = int(input("aの値を入力: "))
+b = int(input("bの値を入力: "))
 
 # TODO
+i=2
+while not a%i==0:
+    if(a==1):
+        break
+    i+=1
+if(i==a):
+    print(str(a)+"は素数です")
+else:
+    print(str(a)+"は素数ではありません")
+
+
+i=2
+while not b%i==0:
+    if(b==1):
+        break
+    i+=1
+if(i==b):
+    print(str(b)+"は素数です")
+else:
+    print(str(b)+"は素数ではありません")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,15 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+import math
+
+a = 0
+b = math.pi/2
+n=100
+h=(b-a)/n
+k=1
+S=0
+while k<=n:
+    S+=h/2*(sin(a+(k-1)*h)+sin(a+k*h))
+    k+=1
+print(S)
